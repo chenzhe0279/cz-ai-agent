041f760ae496c9a9f3e2ab94092d9bb8a4b902f2
czzgods <3302076969@qq.com>
2026-08-02 00:12:28 +0800

    自定义模型对话数据本地文件持久化

--- src/test/java/com/cz/czaiagent/app/LoveAppTest.java
+++ src/test/java/com/cz/czaiagent/app/LoveAppTest.java
@@ -1,12 +1,40 @@
 package com.cz.czaiagent.app;
 
+import cn.hutool.core.lang.UUID;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class LoveAppTest {
 
+    @Resource
+    private LoveApp loveApp;
     @Test
-    void doChat() {
+    void testDoChat() {
+        String chatId = UUID.randomUUID().toString();
+        //第一轮对话
+        String message = "你好，我是编程爱好者陈爱国";
+        String answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        //第二轮对话
+        message = "我想让我喜欢的女孩子也喜欢我";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        //第三轮对话
+        message = "我叫什么名字来着？我刚刚跟说过，帮我回忆一下";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+
+    @Test
+    void doChatWithReport() {
+        String chatId = UUID.randomUUID().toString();
+        //第一轮对话
+        String message = "你好，我是编程爱好者陈爱国,我想找个女朋友，但是我不知道怎么办";
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(loveReport);
     }
 }
